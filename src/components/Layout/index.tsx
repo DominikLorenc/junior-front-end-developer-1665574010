d6375c193d7582c8b47f75320e2b2af2cf6b2b91
Dominik Lorenc <dominiklorenc1270@gmail.com>
2022-10-14 15:50:03 +0200

    prepared configuration project

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss';
+const { Wrapper } = styles;
+
+export const Layout = () => {
+  return (
+    <div className={Wrapper}>
+      <h1>test</h1>
+    </div>
+  );
+};
